95f483628c5f6d60d4442d5c284987c352a62417
cock45 <stardevworld@gmail.com>
2022-02-24 16:08:53 +0900

    conflit fix 1

--- server/src/helpers/rankingsHelperFunctions.js.js
+++ server/src/helpers/rankingsHelperFunctions.js.js
@@ -1,32 +1,11 @@
-/**
- * === HELPER FUNCTIONS FOR : OpenseaScraper.rankings() ===
- * These are all functions that need to be exposed inside
- * puppeteers page.evaluate() function (inside the chromium instance)
- * additionally to the functions we also need a global variable
- * "dict" that holds a dictionary of all scraped collections
- *
- * To make it more readable we outsource this to a seperate file,
- * then load the file via puppeteers page.addScriptTag() function
- */
-
-// initialize dict global variable inside puppeteer chromium instance
-// to save all scraped data
 const dict = {};
 
-// fetches collections that are currently visible on the page
-// and save them to the passed dictionary, with the slug being the key.
-// When a collection is already in the dict it will be overwritten.
 function fetchCollections(dict) {
   const rowsNodeList = document.querySelectorAll('[role=list] > div > a');
   const rows = Array.prototype.slice.call(rowsNodeList);
-  // const nextDataStr = document.getElementById("__NEXT_DATA__").innerText;
-  // const __NEXT_DATA__ = JSON.parse(nextDataStr);
-  // const rows = __NEXT_DATA__.props.relayCache[0][1].json.data.rankings.edges;
 
   rows.forEach(row => {
     const slug = _extractSlug(row);
-    // const obj = row.node;
-    // const slug = obj.slug;
 
     if (slug) { // if slug exists, override dict
       dict[slug] = {
@@ -44,19 +23,11 @@ function fetchCollections(dict) {
     }
   })
 }
-        // nativeCurrency: obj.nativePaymentAsset.symbol,
-        // numOwners: Number(obj.statsV2.numOwners),
-        // totalSupply: Number(obj.statsV2.totalSupply),
-        // oneDayVolume: Number(obj.statsV2.oneDayVolume.unit),
-        // sevenDayVolume: Number(obj.statsV2.sevenDayVolume.unit),
-        // thirtyDayVolume: Number(obj.statsV2.thirtyDayVolume),
-        // thirtyDayChange: Number(obj.statsV2.thirtyDayChange),
-        // totalVolume: Number(obj.statsV2.totalVolume.unit)
+
 function _extractRank(row) {
   try {
-    // return Number(row.querySelector("div > div").innerText);
     return Number(row.innerText.split(/\D/)[0]);
-  } catch(err) {
+  } catch (err) {
     return undefined;
   }
 }
@@ -64,39 +35,36 @@ function _extractSlug(row) {
   try {
     const collectionUrlSplit = row.href.split("/");
     const slug = collectionUrlSplit[collectionUrlSplit.length - 1];
-    // const collectionUrlSplit = row.querySelector('a').href.split("/");
-    // const slug = collectionUrlSplit[collectionUrlSplit.length - 1];
     return slug;
-  } catch(err) {
+  } catch (err) {
     return undefined;
   }
 }
 function _extractLogo(row) {
   try {
     return row.querySelector(".Image--image").src;
-  } catch(err) {
+  } catch (err) {
     return undefined;
   }
 }
 function _extractName(row) {
   try {
-    //return row.querySelector(".Ranking--collection-name-overflow").innerText;
     return row.children[0].children[2].children[0].innerText;
-  } catch(err) {
+  } catch (err) {
     return undefined;
   }
 }
 function _extractFloorPrice(row) { // only ETH floor prices, otherwise uzndefined
   try {
     return row.children[4].children[0].children[1].innerText;
-  } catch(err) {
+  } catch (err) {
     return undefined;
   }
 }
 function _extractVolume(row) {
   try {
     return row.children[1].children[0].children[1].innerText;
-  } catch(err) {
+  } catch (err) {
     return undefined;
   }
 }
@@ -104,28 +72,28 @@ function _extractOneDayChange(row) {
   try {
     // return row.children[0].children[2].children[0].children[0].innerText;
     return row.children[2].children[0].children[1].innerText;
-  } catch(err) {
+  } catch (err) {
     return undefined;
   }
 }
 function _extractSevenDayChange(row) {
   try {
     return row.children[3].children[0].children[0].innerText;
-  } catch(err) {
+  } catch (err) {
     return undefined;
   }
 }
 function _extractOwners(row) {
   try {
     return row.children[5].children[0].innerHTML;
-  } catch(err) {
+  } catch (err) {
     return undefined;
   }
 }
 function _extractItems(row) {
   try {
     return row.children[6].children[0].innerHTML;
-  } catch(err) {
+  } catch (err) {
     return undefined;
   }
 }
